4eb6b608456c72e6f302a3061e0bf98808279ce5
MBESHA_254 <mbesha37@gmail.com>
2025-08-11 13:46:44 +0000

    MAKE IT I AM SAYING THE TWO ICONS YOU JUST ADDED

--- src/components/trade/icons.tsx
+++ src/components/trade/icons.tsx
@@ -7,16 +7,16 @@ export const TradeIcon = () => (
 );
 
 export const SortIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M10 3L10 15M10 15L6 11M10 15L14 11" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M17 21L17 9M17 9L13 13M17 9L21 13" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 3L10 15M10 15L6 11M10 15L14 11" stroke="#888888" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M17 21L17 9M17 9L13 13M17 9L21 13" stroke="#888888" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
 
 export const AddOrderIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8.25 2.25H15.75C19.25 2.25 21.75 4.75 21.75 8.25V15.75C21.75 19.25 19.25 21.75 15.75 21.75H8.25C4.75 21.75 2.25 19.25 2.25 15.75V8.25C2.25 4.75 4.75 2.25 8.25 2.25Z" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M12 8.5V15.5" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M15.5 12H8.5" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M8.25 2.25H15.75C19.25 2.25 21.75 4.75 21.75 8.25V15.75C21.75 19.25 19.25 21.75 15.75 21.75H8.25C4.75 21.75 2.25 19.25 2.25 15.75V8.25C2.25 4.75 4.75 2.25 8.25 2.25Z" stroke="#888888" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M12 8.5V15.5" stroke="#888888" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M15.5 12H8.5" stroke="#888888" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
