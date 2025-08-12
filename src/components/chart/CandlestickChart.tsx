b1d1179b8e4633a1bf48c2415c7c810f03ea924a
MBESHA_254 <mbesha37@gmail.com>
2025-08-12 11:53:47 +0000

    OK DO THIS CHANGE THE CANDLE PATTERN TO WHAT IN THE IMAGE EXACT NOTHING

--- src/components/chart/CandlestickChart.tsx
+++ src/components/chart/CandlestickChart.tsx
@@ -4,17 +4,12 @@
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Cell } from 'recharts';
 
 const data = [
-    { time: '12 Aug 09:00', open: 3343.50, high: 3345.00, low: 3342.50, close: 3344.00 },
-    { time: '12 Aug 09:05', open: 3344.00, high: 3346.50, low: 3343.80, close: 3346.00 },
-    { time: '12 Aug 09:10', open: 3346.00, high: 3348.90, low: 3345.50, close: 3348.20 },
-    { time: '12 Aug 09:15', open: 3348.20, high: 3348.50, low: 3347.00, close: 3347.50 },
-    { time: '12 Aug 09:20', open: 3347.50, high: 3348.00, low: 3345.00, close: 3345.50 },
-    { time: '12 Aug 09:25', open: 3345.50, high: 3347.80, low: 3345.20, close: 3347.15 },
-    { time: '12 Aug 09:31', open: 3347.15, high: 3347.85, low: 3346.00, close: 3346.50 },
-    { time: '12 Aug 09:37', open: 3346.50, high: 3347.00, low: 3345.75, close: 3346.96 },
-    { time: '12 Aug 09:43', open: 3346.96, high: 3347.20, low: 3345.50, close: 3345.80 },
-    { time: '12 Aug 09:49', open: 3345.80, high: 3346.81, low: 3345.12, close: 3346.12 },
-    { time: '12 Aug 09:55', open: 3346.12, high: 3346.52, low: 3344.00, close: 3344.50 },
+    { time: '12 Aug 09:31', open: 3348.55, high: 3348.90, low: 3347.50, close: 3347.50 },
+    { time: '12 Aug 09:37', open: 3347.15, high: 3347.85, low: 3346.00, close: 3346.50 },
+    { time: '12 Aug 09:43', open: 3346.50, high: 3347.00, low: 3345.75, close: 3346.96 },
+    { time: '12 Aug 09:49', open: 3346.96, high: 3347.20, low: 3345.50, close: 3345.80 },
+    { time: '12 Aug 09:55', open: 3345.80, high: 3346.81, low: 3345.12, close: 3346.12 },
+    { time: '12 Aug 10:01', open: 3346.12, high: 3346.52, low: 3344.00, close: 3344.50 },
 ];
 
 const Candlestick = (props: any) => {
