88ea2144ac5a04f8c9e81ff88f5f369b0001b20e
MBESHA_254 <mbesha37@gmail.com>
2025-08-12 12:06:15 +0000

    NOO I WANT THIS EXACT CANDLE STCKS PATTERN IN THE IMAGE

--- src/components/chart/CandlestickChart.tsx
+++ src/components/chart/CandlestickChart.tsx
@@ -4,31 +4,30 @@
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Cell } from 'recharts';
 
 const data = [
-    { time: '12 Aug 09:31', open: 3348.55, high: 3348.90, low: 3347.50, close: 3347.50 },
-    { time: '12 Aug 09:37', open: 3347.15, high: 3347.85, low: 3346.00, close: 3346.50 },
-    { time: '12 Aug 09:43', open: 3346.50, high: 3347.00, low: 3345.75, close: 3346.96 },
-    { time: '12 Aug 09:49', open: 3346.96, high: 3347.20, low: 3345.50, close: 3345.80 },
-    { time: '12 Aug 09:55', open: 3345.80, high: 3346.81, low: 3345.12, close: 3346.12 },
-    { time: '12 Aug 10:01', open: 3346.12, high: 3346.52, low: 3344.00, close: 3344.50 },
-    { time: '12 Aug 10:07', open: 3344.50, high: 3345.20, low: 3343.80, close: 3344.90 },
-    { time: '12 Aug 10:13', open: 3344.90, high: 3345.90, low: 3344.70, close: 3345.60 },
-    { time: '12 Aug 10:19', open: 3345.60, high: 3346.30, low: 3345.10, close: 3345.20 },
-    { time: '12 Aug 10:25', open: 3345.20, high: 3345.80, low: 3344.50, close: 3344.70 },
-    { time: '12 Aug 10:31', open: 3344.70, high: 3345.50, low: 3343.90, close: 3344.20 },
-    { time: '12 Aug 10:37', open: 3344.20, high: 3344.80, low: 3343.50, close: 3343.70 },
-    { time: '12 Aug 10:43', open: 3343.70, high: 3344.60, low: 3343.20, close: 3344.40 },
-    { time: '12 Aug 10:49', open: 3344.40, high: 3345.10, low: 3344.00, close: 3344.90 },
-    { time: '12 Aug 10:55', open: 3344.90, high: 3345.70, low: 3344.50, close: 3345.50 },
-    { time: '12 Aug 11:01', open: 3345.50, high: 3346.20, low: 3345.00, close: 3346.00 },
-    { time: '12 Aug 11:07', open: 3346.00, high: 3346.80, low: 3345.80, close: 3346.50 },
-    { time: '12 Aug 11:13', open: 3346.50, high: 3347.30, low: 3346.30, close: 3347.10 },
-    { time: '12 Aug 11:19', open: 3347.10, high: 3347.90, low: 3346.90, close: 3347.70 },
-    { time: '12 Aug 11:25', open: 3347.70, high: 3348.50, low: 3347.50, close: 3348.30 },
-    { time: '12 Aug 11:31', open: 3348.30, high: 3349.10, low: 3348.10, close: 3348.90 },
-    { time: '12 Aug 11:37', open: 3348.90, high: 3349.70, low: 3348.70, close: 3349.50 },
-    { time: '12 Aug 11:43', open: 3349.50, high: 3350.30, low: 3349.30, close: 3350.10 },
-    { time: '12 Aug 11:49', open: 3350.10, high: 3350.90, low: 3349.90, close: 3350.70 },
-    { time: '12 Aug 11:55', open: 3350.70, high: 3351.50, low: 3350.50, close: 3351.30 },
+    { time: '12 Aug 09:31', open: 3351.50, high: 3351.80, low: 3350.20, close: 3350.50 },
+    { time: '12 Aug 09:37', open: 3350.50, high: 3351.00, low: 3349.50, close: 3349.80 },
+    { time: '12 Aug 09:43', open: 3349.80, high: 3350.10, low: 3348.00, close: 3348.20 },
+    { time: '12 Aug 09:49', open: 3348.20, high: 3349.00, low: 3347.50, close: 3348.80 },
+    { time: '12 Aug 09:55', open: 3348.80, high: 3349.20, low: 3348.00, close: 3348.40 },
+    { time: '12 Aug 10:01', open: 3348.40, high: 3348.90, low: 3347.00, close: 3347.20 },
+    { time: '12 Aug 10:07', open: 3347.20, high: 3348.00, low: 3346.50, close: 3347.80 },
+    { time: '12 Aug 10:13', open: 3347.80, high: 3348.10, low: 3347.00, close: 3347.20 },
+    { time: '12 Aug 10:19', open: 3347.20, high: 3347.50, low: 3346.00, close: 3346.30 },
+    { time: '12 Aug 10:25', open: 3346.30, high: 3346.80, low: 3345.50, close: 3345.80 },
+    { time: '12 Aug 10:31', open: 3344.00, high: 3345.00, low: 3343.50, close: 3344.80 },
+    { time: '12 Aug 10:37', open: 3344.80, high: 3345.50, low: 3344.00, close: 3344.20 },
+    { time: '12 Aug 10:43', open: 3344.20, high: 3344.50, low: 3342.00, close: 3342.50 },
+    { time: '12 Aug 10:49', open: 3342.50, high: 3343.50, low: 3341.00, close: 3343.20 },
+    { time: '12 Aug 10:55', open: 3342.00, high: 3342.80, low: 3339.00, close: 3339.50 },
+    { time: '12 Aug 11:01', open: 3339.50, high: 3341.50, low: 3338.00, close: 3341.00 },
+    { time: '12 Aug 11:07', open: 3341.00, high: 3342.50, low: 3340.50, close: 3341.80 },
+    { time: '12 Aug 11:13', open: 3341.80, high: 3342.20, low: 3339.00, close: 3339.50 },
+    { time: '12 Aug 11:19', open: 3339.50, high: 3345.00, low: 3339.00, close: 3344.50 },
+    { time: '12 Aug 11:25', open: 3344.50, high: 3348.00, low: 3344.00, close: 3347.50 },
+    { time: '12 Aug 11:31', open: 3347.50, high: 3351.00, low: 3347.00, close: 3350.50 },
+    { time: '12 Aug 11:37', open: 3350.50, high: 3352.00, low: 3350.00, close: 3351.50 },
+    { time: '12 Aug 11:43', open: 3351.50, high: 3352.50, low: 3350.80, close: 3351.00 },
+    { time: '12 Aug 11:49', open: 3351.00, high: 3351.80, low: 3350.00, close: 3350.20 },
 ];
 
 const Candlestick = (props: any) => {
@@ -53,8 +52,8 @@ const Candlestick = (props: any) => {
 };
 
 export default function CandlestickChart() {
-  const yMin = Math.min(...data.map(d => d.low)) - 5;
-  const yMax = Math.max(...data.map(d => d.high)) + 5;
+  const yMin = Math.min(...data.map(d => d.low)) - 2;
+  const yMax = Math.max(...data.map(d => d.high)) + 2;
   const yDomain: [number, number] = [yMin, yMax];
   
   const interval = 0.45;
