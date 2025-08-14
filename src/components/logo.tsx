ba1fd0edfb7d7e98149bae308ab0b0c83ddf3ece
MBESHA_254 <mbesha37@gmail.com>
2025-08-14 19:40:21 +0000

    rename the whole app even its app name to MetaTrader 5

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Logo = () => {
     return (
       <div className="flex items-center justify-center h-12 w-12 bg-primary rounded-full text-primary-foreground font-bold text-xl">
-        TV
+        MT
       </div>
     );
 };
