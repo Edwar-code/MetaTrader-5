cbaad859fcd27c9df8489ba6eb1dee3083d6501b
MBESHA_254 <mbesha37@gmail.com>
2025-08-14 10:22:31 +0000

    IN LOT SIZE THE BACKGROUND IS GREYISH BUT IT SHOULD BE WHITE NOT GREYISH

--- src/components/chart/ChartPage.tsx
+++ src/components/chart/ChartPage.tsx
@@ -88,7 +88,7 @@ export default function ChartPage() {
             <span className="text-[22px] font-bold">.{formattedSellPrice.fractional}</span>
           </div>
         </div>
-        <div className="bg-gray-100 px-2 flex items-center justify-center min-w-[140px] flex-grow-[0.4]">
+        <div className="bg-white px-2 flex items-center justify-center min-w-[140px] flex-grow-[0.4]">
           <div className="flex items-center space-x-6">
             <div className="cursor-pointer p-1">
               <div className="w-0 h-0 border-l-[4px] border-l-transparent border-r-[4px] border-r-transparent border-t-[6px] border-t-gray-600"></div>
