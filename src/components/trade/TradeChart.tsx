12e8b6bafade1a6ae0e35eca7297e8cc61826d42
MBESHA_254 <mbesha37@gmail.com>
2025-08-15 10:10:40 +0000

    THE BORDERS FOR THE PRICES HAVE SMOOTH EDGES BUT I WANTED THEM SHARP WIT

--- src/components/trade/TradeChart.tsx
+++ src/components/trade/TradeChart.tsx
@@ -134,7 +134,7 @@ const OrderPriceLabel = ({ viewBox, value, tradeType }: any) => {
         <foreignObject x={width} y={y - 10} width="70" height="20" className="overflow-visible">
           <div
             xmlns="http://www.w3.org/1999/xhtml"
-            className="w-full h-full text-xs flex items-center justify-center rounded-sm text-black bg-white/90 font-semibold border"
+            className="w-full h-full text-xs flex items-center justify-center text-black bg-white/90 font-semibold border"
             style={{ borderColor: color }}
           >
             {value.toFixed(2)}
@@ -154,7 +154,7 @@ const YAxisLabel = ({ viewBox, value }: any) => {
         <foreignObject x={width} y={y - 10} width="70" height="20" className="overflow-visible">
           <div
             xmlns="http://www.w3.org/1999/xhtml"
-            className="w-full h-full text-xs flex items-center justify-center rounded-sm text-white bg-green-500 font-semibold"
+            className="w-full h-full text-xs flex items-center justify-center text-white bg-green-500 font-semibold"
           >
             {value.toFixed(2)}
           </div>
