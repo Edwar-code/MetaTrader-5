5b74a27165a208ad43c914ba8a522328e761bc30
MBESHA_254 <mbesha37@gmail.com>
2025-08-21 14:18:35 +0000

    MAKE IT 190X

--- src/components/chart/ChartPage.tsx
+++ src/components/chart/ChartPage.tsx
@@ -222,7 +222,7 @@ export default function ChartPage() {
             }}
           />
       
-      <div className="absolute bottom-16 w-[210px] flex flex-col items-center z-50" style={{ marginBottom: '11px', left: '200px' }}>
+      <div className="absolute bottom-16 w-[210px] flex flex-col items-center z-50" style={{ marginBottom: '11px', left: '190px' }}>
         {tradeNotifications.map((trade, index) => (
             <TradeNotification 
                 key={trade.id}
