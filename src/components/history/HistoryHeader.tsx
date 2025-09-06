9f80f86071197fa9c14861320e82b0222a7fab50
MBESHA_254 <mbesha37@gmail.com>
2025-09-06 12:27:09 +0000

    THE ONE ON THE LEFT https://on98bvtkqbnonyxs.public.blob.vercel-storage.

--- src/components/history/HistoryHeader.tsx
+++ src/components/history/HistoryHeader.tsx
@@ -4,9 +4,9 @@
 import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Sidebar } from '../trade/Sidebar';
-import { RefreshCw, Calendar } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { SortIcon } from '../trade/icons';
+import Image from 'next/image';
 
 export default function HistoryHeader() {
   const { resolvedTheme } = useTheme();
@@ -43,13 +43,13 @@ export default function HistoryHeader() {
         </div>
         <div className="flex items-center gap-[18px]">
           <Button variant="ghost" size="icon" className="h-10 w-10 text-muted-foreground">
-            <RefreshCw size={22} />
+            <Image src="https://on98bvtkqbnonyxs.public.blob.vercel-storage.com/WhatsApp%20Image%202025-09-06%20at%2015.15.09_aa47dbcc.jpg" alt="Refresh History" width={22} height={22} />
           </Button>
           <Button variant="ghost" size="icon" className="h-10 w-10 text-muted-foreground hover:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 active:bg-transparent">
             <SortIcon />
           </Button>
           <Button variant="ghost" size="icon" className="h-10 w-10 text-muted-foreground">
-            <Calendar size={22} />
+            <Image src="https://on98bvtkqbnonyxs.public.blob.vercel-storage.com/WhatsApp%20Image%202025-09-06%20at%2015.15.34_492037f3.jpg" alt="Filter History" width={22} height={22} />
           </Button>
         </div>
       </div>
