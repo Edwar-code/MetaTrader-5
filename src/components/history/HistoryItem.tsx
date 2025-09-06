ad73608e39e6716f073666dbd15ed3634542ded9
MBESHA_254 <mbesha37@gmail.com>
2025-09-06 13:00:54 +0000

    NOW -8PX

--- src/components/history/HistoryItem.tsx
+++ src/components/history/HistoryItem.tsx
@@ -80,7 +80,7 @@ export default function HistoryItem({ position }: HistoryItemProps) {
                  <span className="text-[13.5px] text-muted-foreground">
                     {format(new Date(position.closeTime * 1000), 'yyyy.MM.dd HH:mm:ss')}
                 </span>
-                <span className={`block text-[13.5px] font-bold`} style={{ color: profitColor, position: 'relative', top: '-10px' }}>
+                <span className={`block text-[13.5px] font-bold`} style={{ color: profitColor, position: 'relative', top: '-8px' }}>
                     {profitString}
                 </span>
             </div>
